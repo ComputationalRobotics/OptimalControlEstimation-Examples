c568a00b3d0518978a9750bb2dae5564f830ac4e
Heng Yang <hengyang@Hengs-MacBook-Pro.local>
2023-07-12 22:27:09 -0400

    update sos code

--- cartpole_sos_observer.m
+++ cartpole_sos_observer.m
@@ -10,11 +10,13 @@ e_bound = 100;
 ns = 6;
 ny = 4;
 
-deg_Q = 2;
-deg_V = 2;
-deg_M = 2;
-kappa = 2;
 gamma = 0.1;  % TODO: update
+deg_V = 2;
+deg_K = 2;
+kappa_plus = 0;
+
+deg_Q = deg_V - 2;
+deg_M = deg_Q + deg_K;
 
 e = mpvar('e',[ns,1]);
 x = mpvar('x',[ns,1]);
@@ -24,6 +26,20 @@ C = [1, 0, 0, 0, 0, 0;
      0, 0, 0, 0, 0, 1];
 Ce = C*e;   
 Cx = C*x;
+delta_f = cartpole_f(x+e,l) - cartpole_f(x,l);
+deg_delta_f = delta_f.maxdeg;
+
+h = [x(3)^2 + x(4)^2 - 1;
+    x(6)*m_c/m_p + x(6)*x(3)^2 - 1];
+
+g = [1;- e'*e + e_bound^2];
+
+max_deg = max([deg_V - 1 + deg_delta_f,2+deg_M,h.maxdeg,g.maxdeg]);
+
+% minimum relaxation order 
+kappa   = ceil(max_deg / 2) + kappa_plus;
+
+fprintf("deg_V: %d, deg_K: %d, kappa: %d.\n",deg_V,deg_K,kappa);
 
 prog = sosprogram([e;x]);
 
@@ -31,23 +47,27 @@ prog = sosprogram([e;x]);
 [prog,Q] = sospolymatrixvar(prog,monomials(Ce,0:deg_Q),[ns,ns],'symmetric');
 [prog,M] = sospolymatrixvar(prog,monomials([Ce;Cx],0:deg_M),[ns,ny]);
 
-h1 = x(3)^2 + x(4)^2 - 1;
-deg_h1 = h1.maxdeg;
-h2 = x(6)*m_c/m_p + x(6)*x(3)^2 - 1;
-deg_h2 = h2.maxdeg;
-
-g1 = - (e'*e - e_bound^2);
-deg_g1 = g1.maxdeg;
 
 [prog,sig0] = sossosvar(prog,monomials([e;x],0:kappa));
-[prog,sig1] = sossosvar(prog,monomials([e;x],0:floor((2*kappa-deg_g1)/2)));
-[prog,lam1] = sospolyvar(prog,monomials([e;x],0:(2*kappa-deg_h1)));
-[prog,lam2] = sospolyvar(prog,monomials([e;x],0:(2*kappa-deg_h2)));
 
-eq = e'*Q*(cartpole_f(x+e,l) - cartpole_f(x,l)) + e'*M*(Ce) + ...
-    sig0 + lam1 * h1 + lam2 * h2 + gamma * V;
+lams = [];
+for i = 1:length(h)
+    deg_hi = h(i).maxdeg;
+    [prog,lami] = sospolyvar(prog,monomials([e;x],0:(2*kappa-deg_hi)));
+    lams = [lams;lami];
+end
+sigs = [];
+for i = 1:length(g)
+    deg_gi = g(i).maxdeg;
+    [prog,sigi] = sossosvar(prog,monomials([e;x],0:floor((2*kappa-deg_gi)/2)));
+    sigs = [sigs;sigi];
+end
 
-eps = 10^-2;
+eps = 0.1;
+
+eq = e'*(Q+eps*eye(ns))*delta_f + e'*M*(Ce) + gamma * V + ...
+    sigs'*g + lams'*h;
+
 prog = soseq(prog,eq);
 prog = sosmatrixineq(prog,Q);
 prog = soseq(prog,diff(V,e)-e'*(Q+eps*eye(ns)));
@@ -66,7 +86,7 @@ sol.M = cleanpoly(sosgetsol(prog,M),threshold);
 % where a = 1/(m_c/m_p+sin(theta)^2)
 function f = cartpole_f(x,l)
 f = [x(2);
-    x(6) * x(3)*l*x(5)^2;
+    x(6)*x(3)*l*x(5)^2;
     x(4)*x(5);
     -x(3)*x(5);
     -x(6)*x(5)^2*x(3)*x(4);
--- pendulum_sos_observer.m
+++ pendulum_sos_observer.m
@@ -7,17 +7,32 @@ addpath(genpath(mosekpath))
 
 m = 1; g = 9.8; l = 1; b = 0.1;
 
-deg_Q = 2;
-deg_V = 2;
-deg_M = 2;
-kappa = 2;
-gamma = 0.1;
+gamma = 0.2; % desired exponential rate
+deg_V = 2; % user-chosen degree of V
+deg_K = 2; % user-chosen degree of K
+kappa_plus = 0; % if >0 then go above the minimum relaxation order
+
+% compute other degrees
+deg_Q = deg_V - 2;
+deg_M = deg_Q + deg_K;
 
 e = mpvar('e',[3,1]);
 x = mpvar('x',[3,1]);
 C = [1, 0, 0; 0, 1, 0];
 Ce = C*e;
 Cx = C*x;
+h = x(1)^2 + x(2)^2 - 1;
+deg_h = h.maxdeg;
+eps = 0.1;
+
+delta_f = pendulum_f(x+e,m,b,l) - pendulum_f(x,m,b,l);
+deg_delta_f = delta_f.maxdeg;
+
+max_deg = max([deg_V - 1 + deg_delta_f,2+deg_M,deg_h]);
+% minimum relaxation order 
+kappa   = ceil(max_deg / 2) + kappa_plus;
+
+fprintf("deg_V: %d, deg_K: %d, kappa: %d.\n",deg_V,deg_K,kappa);
 
 prog = sosprogram([e;x]);
 
@@ -25,25 +40,24 @@ prog = sosprogram([e;x]);
 [prog,Q] = sospolymatrixvar(prog,monomials(Ce,0:deg_Q),[3,3],'symmetric');
 [prog,M] = sospolymatrixvar(prog,monomials([Ce;Cx],0:deg_M),[3,2]);
 
-h = x(1)^2 + x(2)^2 - 1;
-deg_h = h.maxdeg;
-[prog,sig0] = sossosvar(prog,monomials([e;x],kappa));
+[prog,sig0] = sossosvar(prog,monomials([e;x],0:kappa));
 [prog,lam1] = sospolyvar(prog,monomials([e;x],0:(2*kappa-deg_h)));
 
-eq = e'*Q*(pendulum_f(x+e,m,b,l) - pendulum_f(x,m,b,l)) + e'*M*(Ce) + ...
+eq = e'*(Q+eps*eye(3))*delta_f + e'*M*(Ce) + ...
     sig0 + lam1 * h + gamma * V;
 
 prog = soseq(prog,eq);
 prog = sosmatrixineq(prog,Q);
-prog = soseq(prog,diff(V,e)-e'*Q);
+prog = soseq(prog,diff(V,e)-e'*(Q+eps*eye(3)));
 prog = soseq(prog,subs(V,e,zeros(3,1)));
-% prog = sossetobj(prog,0);
+
 options.solver = 'mosek';
 prog = sossolve(prog,options);
 
-sol.V = sosgetsol(prog,V);
-sol.Q = sosgetsol(prog,Q);
-sol.M = sosgetsol(prog,M);
+threshold = 1e-6;
+sol.V = cleanpoly(sosgetsol(prog,V),threshold);
+sol.Q = cleanpoly(sosgetsol(prog,Q),threshold);
+sol.M = cleanpoly(sosgetsol(prog,M),threshold);
 
 
 
