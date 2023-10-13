4d02d679d5f94c458f9251628c961849f23682d1
Heng Yang <hengyang@dhcp-10-250-210-66.harvard.edu>
2023-10-12 20:26:09 -0400

    add code

--- double_integrator_mpc/double_integrator_stability.m
+++ double_integrator_mpc/double_integrator_stability.m
@@ -5,14 +5,18 @@ Q = [1,0;0,0]; R = 1;
 
 [~,S,~] = dlqr(A,B,Q,R,zeros(2,1));
 
-P = S;
-N = 1;
+% P = 0.1*eye(2);
+P = 1*S;
+N = 3;
 
-x = [5;10];
+x = [2;1];
 x_traj = x;
 u_traj = [];
+rho_traj = [];
 for t = 1:100
-    [uopt,info] = double_integrator_rhc(A,B,P,Q,R,N,x);
+%     [uopt,info] = double_integrator_rhc(A,B,P,Q,R,N,x);
+    uopt = double_integrator_rhc_cvx(A,B,P,Q,R,N,x);
+    rho = check_clf(x,A,B,P,Q,R);
 
     xnew = A*x + B*uopt(1);
 
@@ -29,10 +33,76 @@ for t = 1:100
     x = xnew;
     x_traj = [x_traj,x];
     u_traj = [u_traj,uopt(1)];
+    rho_traj = [rho_traj;rho];
 end
 
-figure; plot(x_traj');
+%% plot
+figure; plot(x_traj','LineWidth',2);
+figure; plot(rho_traj,'LineWidth',2);
 
+
+
+%% helper functions
+function rho = check_clf(x,A,B,P,Q,R)
+cvx_begin
+    variable u(1,1)
+    xnew = A*x+B*u;
+    minimize ((xnew'*P*xnew) - (x'*P*x) + (x'*Q*x) + (u'*R*u));
+    subject to
+        u >= -1;
+        u <= 1;
+cvx_end
+
+rho = cvx_optval;
+
+end
+
+
+
+
+function uopt = double_integrator_rhc_cvx(A,B,P,Q,R,N,xt)
+cvx_begin
+    variable u(N,1)
+    variable x(2,N)
+    f = cost(x,u,xt,P,Q,R,N);
+    minimize f
+    subject to
+        for k = 0:N-1
+            u(k+1) >= -1
+            u(k+1) <= 1
+            if k == 0
+                x(:,k+1) == A*xt + B*u(k+1);
+            else
+                x(:,k+1) == A*x(:,k) + B*u(k+1);
+            end
+        end
+cvx_end
+
+if cvx_status == "Infeasible"
+    uopt = [];
+else
+    uopt = u;
+end
+
+end
+
+
+function f = cost(x,u,xt,P,Q,R,N)
+f = 0;
+for k = 0:N-1
+    uk = u(k+1);
+    if k==0
+        xk = xt;
+    else
+        xk = x(:,k);
+    end
+    f = f + xk'*Q*xk + uk'*R*uk;
+end
+
+f = f + x(:,N)'*P*x(:,N);
+end
+
+%% implemented using quadprog
 function [uopt,info] = double_integrator_rhc(A,B,P,Q,R,N,xt)
 % decision variable
 % [u(0),...,u(N-1),x(1),...,x(N)]
